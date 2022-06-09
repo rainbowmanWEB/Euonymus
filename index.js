a1b63a5965298e3fb0fe47cbcca83cd15c2472db
rainbowmanWEB <94617890+rainbowmanWEB@users.noreply.github.com>
2022-06-09 04:18:44 +0300

    add header
    add header

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-import './scss/main.scss';
-import './js/main.js';
 import './vendor/bootstrap5/bootstrap.bundle.min.js';
 import './vendor/bootstrap5/bootstrap.min.css';
+import './scss/main.scss';
+import './js/main.js';
+import './images/google-plus.svg';
+import './images/pinterest.svg';
+import './images/rss.svg';
+import './images/twitter.svg';
